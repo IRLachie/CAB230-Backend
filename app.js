3596a55deb072c39e406affc51ecf8e5772aa435
Lachie King <lachy.m@hotmail.com>
2022-05-29 03:42:29 +1000

    File cleanup

--- app.js
+++ app.js
@@ -24,10 +24,6 @@ var meRouter = require("./routes/me");
 
 var app = express();
 
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
-
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
